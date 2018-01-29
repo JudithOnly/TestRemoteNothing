0460861e1a332cd048174a57ccf6d1d264a28b23
judith <>
2018-01-29 19:31:11 +0800

    初始化

---
+++ testRemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testRemo
+//
+//  Created by judith on 2018/1/29.
+//  Copyright © 2018年 BS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testRemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testRemo
+//
+//  Created by judith on 2018/1/29.
+//  Copyright © 2018年 BS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
